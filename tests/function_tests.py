41580ed9533d87537a3dbd6b0481ab447094c4af
Ashfaq Ur Rahman <anashfire0@gmail.com>
2019-09-28 08:35:32 +0530

    functional test checks for the input

--- tests/function_tests.py
+++ tests/function_tests.py
@@ -1,5 +1,7 @@
+import time
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,6 +18,30 @@ class NewVisitorTest(unittest.TestCase):
 
         #Basava notices page title and headers of the main page
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To Do', header_text)
+    
+        #He sees a placeholder
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual('Enter a to-do item', 
+                        inputbox.get_attribute('placeholder')
+                        )
+        
+        #he types beer in the list
+        inputbox.send_keys('Buy a Beer')
+
+        #he presses Enter, page updates, 1. Buy a beer is reflected
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Buy a Beer' for row in rows)
+        )
+
+
+        #there's still text inviting her to add more to-dos
         self.fail('Finish the test.')
 
 if __name__ == '__main__':
