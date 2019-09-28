0fa233fc1e57cd9886879455ef8eb1311f326fd5
Ashfaq Ur Rahman <anashfire0@gmail.com>
2019-09-28 09:13:36 +0530

    Front page HTML now generated from a template

--- tests/function_tests.py
+++ tests/function_tests.py
@@ -36,8 +36,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
+        time.sleep(20)
         self.assertTrue(
-            any(row.text == '1. Buy a Beer' for row in rows)
+            any(row.text == '1. Buy a Beer' for row in rows),
+            "New to-do entry did not appear in the table"
         )
 
 
