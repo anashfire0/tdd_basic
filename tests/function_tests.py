95c28d52fa3424e4f16581be35f363d654c0d0e4
Ashfaq Ur Rahman <anashfire0@gmail.com>
2019-09-29 09:49:48 +0530

    refactored functional tests

--- tests/function_tests.py
+++ tests/function_tests.py
@@ -11,6 +11,19 @@ class NewVisitorTest(unittest.TestCase):
     
     def tearDown(self):
         self.browser.quit()
+    
+    def check_for_row_in_a_list(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+    
+    def insert_a_to_do_item(self, to_do_text):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(to_do_text)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Basava goes to the url of the website
@@ -27,26 +40,16 @@ class NewVisitorTest(unittest.TestCase):
                         inputbox.get_attribute('placeholder')
                         )
         
-        #he types beer in the list
-        inputbox.send_keys('Buy a Beer')
-
-        #he presses Enter, page updates, 1. Buy a beer is reflected
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-
-
-        #do the same for buy a peacock
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy a Peacock.')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
+        #he enters 1st to do item
+        self.insert_a_to_do_item('Buy a Beer')
+        #he enters 2nd to do item
+        self.insert_a_to_do_item('Buy a Peacock')
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1. Buy a Beer', [row.text for row in rows])
-        self.assertIn('2. Buy a Peacock', [row.text for row in rows])
+        #he sees the 1st table field
+        self.check_for_row_in_a_list('1. Buy a Beer')
+        #he sees the 2nd table field
+        self.check_for_row_in_a_list('2. Buy a Peacock')
+
 
 
         #there's still text inviting her to add more to-dos
